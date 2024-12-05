2b20e79dfca2879cf7561fafb2672d32c5ae55a8
Dima-Korchukov <165017018+Dima-Korchukov@users.noreply.github.com>
2024-12-05 14:10:56 +0200

    add lab 8

---
+++ Lab8/script.js
@@ -0,0 +1,10 @@
+const playButton = document.getElementById('play-button');
+const video = document.getElementById('background-video');
+
+playButton.addEventListener('click', () => {
+  if (video.paused) {
+    video.style.display = 'block'; // Показуємо відео
+    video.play();
+    playButton.style.display = 'none'; // Приховуємо кнопку після запуску
+  }
+});
